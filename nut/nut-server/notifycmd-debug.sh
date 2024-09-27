725508aa10e5b27b839f11d6c06f84b0758a17b9
denis <denis.pollini.90@gmail.com>
2024-09-27 15:36:29 +0200

    edit notifycmd script nut

--- nut/nut-server/notifycmd-debug.sh
+++ nut/nut-server/notifycmd-debug.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 set -e
 
+# File di log per debug
+LOGFILE="/var/log/ups-email-notifier.log"
+echo "$(date) - Script invoked with argument: $1" >> $LOGFILE
+
 # Email
 EMAIL_RECIPIENT=$(grep '^user:root@pam' /etc/pve/user.cfg | awk -F':' '{print $7}')
+echo "$(date) - Email recipient: $EMAIL_RECIPIENT" >> $LOGFILE
 
 # Define the message to send based on argument containing relevant string
 if [[ "$1" == *"on battery"* ]]; then
     SUBJECT="UPS on Battery"
     BODY="The UPS is running on battery."
+    echo "$(date) - UPS is running on battery" >> $LOGFILE
 elif [[ "$1" == *"on line power"* ]]; then
     SUBJECT="UPS Power Restored"
     BODY="The UPS is back on power."
+    echo "$(date) - UPS is back on power" >> $LOGFILE
 else
+    echo "$(date) - Invalid state or argument: $1" >> $LOGFILE
     exit 0
 fi
 
 # Send the email
 echo "$BODY" | mail -s "$SUBJECT" $EMAIL_RECIPIENT
+echo "$(date) - Email sent" >> $LOGFILE
--- nut/nut-server/notifycmd.sh
+++ nut/nut-server/notifycmd.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-#Email
+# Email
 EMAIL_RECIPIENT=$(grep '^user:root@pam' /etc/pve/user.cfg | awk -F':' '{print $7}')
 
-# Define the message to send
-if [ "$1" = "on line power" ]; then
-    SUBJECT="UPS Power Restored"
-    BODY="The UPS is back on power."
-elif [ "$1" = "on battery" ]; then
+# Define the message to send based on argument containing relevant string
+if [[ "$1" == *"on battery"* ]]; then
     SUBJECT="UPS on Battery"
     BODY="The UPS is running on battery."
+elif [[ "$1" == *"on line power"* ]]; then
+    SUBJECT="UPS Power Restored"
+    BODY="The UPS is back on power."
 else
     exit 0
 fi
